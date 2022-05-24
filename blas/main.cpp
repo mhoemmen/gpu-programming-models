72b40ff36211d5393e37e2563ec276e4377a36c9
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2022-05-24 13:45:34 -0400

    Added kokkos-kernels example

--- blas/main.cpp
+++ blas/main.cpp
@@ -1,39 +1,44 @@
 #include<cassert>
-#include<vector>
-
-extern "C" {
-  void dcopy_(const int* n, const double* x, const int* incx, double* y, const int* incy);
-
-  void daxpy_(const int* n, const double* alpha, 
-              const double* x, const int* incx,
-              double* y, const int* incy);
-} // end extern
+#include<Kokkos_Core.hpp>
+#include<KokkosBlas1_update.hpp>
 
 int main(int argc, char* argv[]) {
-    using std::vector;
+    using Kokkos::View;
+    using Kokkos::create_mirror_view;
+    using Kokkos::deep_copy;
 
-    const int n = 100'000'000;
-    const int ONE = 1;
-    const double alpha = 1.0;
-    vector<double> x(n), y(n), z(n);
+    Kokkos::initialize(argc, argv);
+    { // View destructors must be invoked before calling finalize, 
+      // so it is common to place code in a block.
+        const int n = 100'000'000;
+        View<double*> x("x", n), y("y", n), z("z", n);
 
-    // Initialize x and y
-    for(int i=0; i<n; i++) {
-        x[i] = i;
-        y[i] = n-i;
+        auto h_x = create_mirror_view(x);
+        auto h_y = create_mirror_view(y);
+        auto h_z = create_mirror_view(z);
+
+        // Initialize x and y on host
+        for(int i=0; i<n; i++) {
+            h_x[i] = i;
+            h_y[i] = n-i;
+        }
+
+        // Copy data to device
+        deep_copy(x, h_x);
+        deep_copy(y, h_y);
+
+        // Compute sum of x and y
+        KokkosBlas::update(1.0, x, 1.0, y, 0.0, z);
+
+        // Copy data to host
+        deep_copy(h_z, z);
+
+        // Assert that the sum is correct
+        for(int i=0; i<n; i++) {
+            assert(h_z[i] == n);
+        }
     }
-
-    // Compute sum of x and y
-    dcopy_(&n, x.data(), &ONE, z.data(), &ONE);
-    daxpy_(&n, &alpha, y.data(), &ONE, z.data(), &ONE);
-    for(int i=0; i<n; i++) {
-        z[i] = x[i] + y[i];
-    }
-
-    // Assert that the sum is correct
-    for(int i=0; i<n; i++) {
-        assert(z[i] == n);
-    }
+    Kokkos::finalize();
 
     return 0;
 }
