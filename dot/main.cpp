7ba174afd097e864419a6a944a9fd3fcb502f60c
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2022-05-26 13:30:59 -0400

    Added CPU dot product example

---
+++ dot/main.cpp
@@ -0,0 +1,26 @@
+#include<cassert>
+#include<vector>
+
+int main(int argc, char* argv[]) {
+    using std::vector;
+
+    const int n = 1e8;
+    vector<double> x(n), y(n);
+
+    // Initialize x and y
+    for(int i=0; i<n; i++) {
+        x[i] = i+1;
+        y[i] = 1.0/x[i];
+    }
+
+    // Compute dot product
+    double sum = 0.0;
+    for(int i=0; i<n; i++) {
+        sum += x[i] * y[i];
+    }
+
+    // Assert that the sum is correct
+    assert(sum == n);
+
+    return 0;
+}
