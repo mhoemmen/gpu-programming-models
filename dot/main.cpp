c2c6ef744f0298e9a8b2e19b35ecb28ff9c78fb4
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2022-05-26 15:19:45 -0400

    Added kokkos dot product example

--- dot/main.cpp
+++ dot/main.cpp
@@ -1,26 +1,41 @@
 #include<cassert>
-#include<vector>
+#include<Kokkos_Core.hpp>
 
 int main(int argc, char* argv[]) {
-    using std::vector;
+    using Kokkos::View;
+    using Kokkos::create_mirror_view;
+    using Kokkos::deep_copy;
+    using Kokkos::parallel_reduce;
 
-    const int n = 100'000'000;
-    vector<double> x(n), y(n);
+    Kokkos::initialize(argc, argv);
+    { // View destructors must be invoked before calling finalize, 
+      // so it is common to place code in a block.
+        const int n = 100'000'000;
+        View<double*> x("x", n), y("y", n);
 
-    // Initialize x and y
-    for(int i=0; i<n; i++) {
-        x[i] = i+1;
-        y[i] = 1.0/x[i];
+        auto h_x = create_mirror_view(x);
+        auto h_y = create_mirror_view(y);
+
+        // Initialize x and y on host
+        for(int i=0; i<n; i++) {
+            h_x[i] = i+1;
+            h_y[i] = 1.0/h_x[i];
+        }
+
+        // Copy data to device
+        deep_copy(x, h_x);
+        deep_copy(y, h_y);
+
+        // Compute dot product
+        double sum = 0.0;
+        parallel_reduce("dot", n, KOKKOS_LAMBDA (const int& i, double& lsum ) {
+            lsum += x[i] * y[i];
+        }, sum);
+
+        // Assert that the sum is correct
+        assert(sum == n);
     }
-
-    // Compute dot product
-    double sum = 0.0;
-    for(int i=0; i<n; i++) {
-        sum += x[i] * y[i];
-    }
-
-    // Assert that the sum is correct
-    assert(sum == n);
+    Kokkos::finalize();
 
     return 0;
 }
