e252b0d20d76f5d0cf3404b2dbe3423a360bd895
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2022-06-03 13:55:15 -0400

    Added gemv example
    Demonstrates the use of multidimensional arrays

---
+++ gemv/main.cpp
@@ -0,0 +1,35 @@
+#include<cassert>
+#include<vector>
+
+int main(int argc, char* argv[]) {
+    using std::vector;
+
+    const int n = 10000;
+    vector<double> x(n), y(n), A(n*n, 0.0);
+
+    // Initialize x
+    for(int i=0; i<n; i++) {
+        x[i] = i;
+    }
+
+    // Initialize A to the identity matrix
+    for(int i=0; i<n; i++) {
+        A[i*(n+1)] = 1.0;
+    }
+
+    // Compute matrix-vector product
+    for(int r=0; r<n; r++) {
+        y[r] = 0.0;
+        for(int c=0; c<n; c++) {
+            // Note that we have hard-coded row-major storage here
+            y[r] += A[r*n+c] * x[c];
+        }
+    }
+
+    // Assert that the product is correct
+    for(int i=0; i<n; i++) {
+        assert(y[i] == i);
+    }
+
+    return 0;
+}
