bbd3ade78c97b29571b9d9580a264bf96cc5af2c
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2022-06-07 16:12:40 -0400

    Added kokkos matvec example

--- gemv/main.cpp
+++ gemv/main.cpp
@@ -1,35 +1,58 @@
 #include<cassert>
-#include<vector>
+#include<Kokkos_Core.hpp>
 
 int main(int argc, char* argv[]) {
-    using std::vector;
+    typedef Kokkos::TeamPolicy<>               team_policy;
+    typedef Kokkos::TeamPolicy<>::member_type  member_type;
 
-    const int n = 10000;
-    vector<double> x(n), y(n), A(n*n, 0.0);
+    using Kokkos::AUTO;
+    using Kokkos::View;
+    using Kokkos::create_mirror_view;
+    using Kokkos::deep_copy;
+    using Kokkos::parallel_for;
 
-    // Initialize x
-    for(int i=0; i<n; i++) {
-        x[i] = i;
-    }
+    Kokkos::initialize(argc, argv);
+    { // View destructors must be invoked before calling finalize, 
+      // so it is common to place code in a block.
+        const int n = 10000;
+        View<double*> x("x", n), y("y", n);
+        View<double**> A("A", n, n);
 
-    // Initialize A to the identity matrix
-    for(int i=0; i<n; i++) {
-        A[i*(n+1)] = 1.0;
-    }
+        auto h_x = create_mirror_view(x);
+        auto h_y = create_mirror_view(y);
+        auto h_A = create_mirror_view(A);
 
-    // Compute matrix-vector product
-    for(int r=0; r<n; r++) {
-        y[r] = 0.0;
-        for(int c=0; c<n; c++) {
-            // Note that we have hard-coded row-major storage here
-            y[r] += A[r*n+c] * x[c];
+        // Initialize x on host
+        for(int i=0; i<n; i++) {
+            h_x[i] = i;
+        }
+
+        // Initialize A to the identity matrix on host
+        for(int i=0; i<n; i++) {
+            h_A(i,i) = 1.0;
+        }
+
+        // Copy data to device
+        deep_copy(x, h_x);
+        deep_copy(A, h_A);
+
+        // Compute matrix-vector product
+        parallel_for("gemv", team_policy(n, AUTO), KOKKOS_LAMBDA(const member_type &teamMember) {
+            int r = teamMember.league_rank();
+            Kokkos::parallel_reduce(Kokkos::TeamThreadRange( teamMember, n ), [&] ( const int c, double &localSum ) {
+                localSum += A(r,c) * x(c);
+            }, y(r));
+        });
+
+        // Copy data to host
+        deep_copy(h_y, y);
+
+        // Assert that the product is correct
+        for(int i=0; i<n; i++) {
+            assert(h_y[i] == i);
         }
     }
-
-    // Assert that the product is correct
-    for(int i=0; i<n; i++) {
-        assert(y[i] == i);
-    }
+    Kokkos::finalize();
 
     return 0;
 }
