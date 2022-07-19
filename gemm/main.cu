648c00aaef3c3da43f09863e5dc8a310183d0523
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2022-07-19 17:11:16 -0400

    cuda matrix multiplication uses blocks
    The matrix is now stored in a block format to prepare for GPU
    offloading. No offloading is currently performed.

--- gemm/main.cu
+++ gemm/main.cu
@@ -1,8 +1,14 @@
+// Perform a block multiplication of two matrices
+// For simplicity, we assume the number of blocks evenly divide the number of rows
+// We use block column major storage, and column major within blocks
+
 #include<cassert>
 #include<random>
 #include<vector>
 
-constexpr auto n = 2000;
+constexpr auto n = 20;  // Number of rows
+constexpr auto nb = 1;   // Number of blocks in a given direction
+constexpr auto bs = n/nb; // Block size
 
 extern "C" {
   void dgemm_(const char* transa, const char* transb, const int* m, const int* n, const int* k,
@@ -11,9 +17,17 @@ extern "C" {
               const double* beta, double* C, const int* ldc);
 } // end extern
 
+// Returns the starting index for a given block
+int getStartingIndex(int blockrow, int blockcol) {
+    return (blockrow + blockcol*nb)*bs*bs;
+}
+
 int getIndex(int row, int col) {
-    // Note that we have hard coded column major storage here
-    return row + col*n;
+    int br = row / bs;
+    int r = row % bs;
+    int bc = col / bs;
+    int c = col % bs;
+    return nb*bs*bs*bc + bs*bs*br + bs*c + r;
 }
 
 int main(int argc, char* argv[]) {
@@ -45,10 +59,24 @@ int main(int argc, char* argv[]) {
     }
 
     // Compute gold standard using BLAS
+    // Because we're using a strange storage format, this takes several BLAS calls
     constexpr auto trans = 'N';
     constexpr auto alpha = 1.0;
-    constexpr auto beta = 0.0;
-    dgemm_(&trans, &trans, &n, &n, &n, &alpha, A.data(), &n, B.data(), &n, &beta, Cgs.data(), &n);
+    constexpr auto beta = 1.0;
+    for(int rb=0; rb<nb; rb++) {
+        for(int cb=0; cb<nb; cb++) {
+            double* cptr = Cgs.data() + getStartingIndex(rb,cb);
+            // Zero out the C block
+            for(int i=0; i<bs; i++) {
+                cptr[i] = 0.0;
+            }
+            for(int i=0; i<nb; i++) {
+                double* aptr = A.data() + getStartingIndex(rb,i);
+                double* bptr = B.data() + getStartingIndex(i,cb);
+                dgemm_(&trans, &trans, &bs, &bs, &bs, &alpha, aptr, &bs, bptr, &bs, &beta, cptr, &bs);
+            }
+        }
+    }
 
     // Assert that the product is correct
     for(int i=0; i<n*n; i++) {
